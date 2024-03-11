d09d2f28808802fcdec48187a1ce3d81c5f6c1be
Jskarie <Jskarie@gmail.com>
2024-03-11 07:10:26 -0500

    Removed duplicated code Ch24.4

--- code/chapter_24_example_04.py
+++ code/chapter_24_example_04.py
@@ -41,11 +41,6 @@ def add_middleware_to_request(request, middleware_class):
     middleware.process_request(request)
     return request
 
-def add_middleware_to_response(request, middleware_class):
-    middleware = middleware_class()
-    middleware.process_response(request)
-    return request
-
 class SavoryIceCreamTest(TestCase):
     def setUp(self):
         # Every test needs access to the request factory.
